cfa2c17de347e819ab4ceeb1f867b7ddd0d0881a
Joseph Garba <josephgarba97@gmail.com>
2024-11-02 10:45:25 +0100

    Add Trustees section

--- frontend/src/components/Trustees.jsx
+++ frontend/src/components/Trustees.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Trustees = () => {
   return (
-    <div>
-      
+    <div className='my-16 text-center'>
+      <img className='w-20' src={assets.ellipse_1} alt="" />
+      <p>Trusted by 100+ Companies across the globe!</p>
+      <div className='flex items-center justify-around gap-10 my-9'>
+        <img className='w-24' src={assets.google} alt="" />
+        <img className='w-24' src={assets.amazon} alt="" />
+        <img className='w-24' src={assets.logitech} alt="" />
+        <img className='w-24' src={assets.spotify} alt="" />
+        <img className='w-24' src={assets.samsung} alt="" />
+        <img className='w-24' src={assets.netflix} alt="" />
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import HeaderBg from "../components/HeaderBg";
 import Display from "../components/Display";
+import Trustees from "../components/Trustees";
 
 const Home = () => {
   return (
     <>
       <HeaderBg />
       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-        
+        <Trustees />
       </div>
     </>
   );
