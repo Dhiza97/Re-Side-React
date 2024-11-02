cdc20b6ec638b432e202dd4fcdbf8f8b8d786d86
Joseph Garba <josephgarba97@gmail.com>
2024-11-02 11:36:11 +0100

    Add Who We Are section

--- frontend/src/components/WhoWeAre.jsx
+++ frontend/src/components/WhoWeAre.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import Heading from "./Heading";
+import { assets } from "../assets/assets";
 
 const WhoWeAre = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex gap-9 my-20">
+      <div className="w-1/2">
+        <Heading text={"WHO ARE WE"} />
+        <p className="text-4xl font-medium text-[#0b090a] leading-[2.8rem]">
+          Assisting individuals in locating <br />
+          the appropriate real estate.
+        </p>
+        <p className=" text-sm mt-3 text-[#808080]">
+          Empowering clients to find their ideal properties, our real estate
+          platform specializes in personalized assistance. With a keen
+          understanding of market dynamics, we streamline the process of
+          locating suitable real estate options for individuals.
+        </p>
 
-export default WhoWeAre
+        <div className="flex flex-col gap-5 mt-5">
+          <div className="flex items-center justify-center p-5 bg-white shadow-lg rounded-full w-[450px] gap-4">
+            <img className="w-10" src={assets.icon_smart_home} alt="" />
+            <div>
+              <p className="text-[#4361ee] mb-2">Finding the right location</p>
+              <p className="text-sm">Discovering the perfect location is paramount in real estate.</p>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center p-5 bg-white shadow-lg rounded-full w-[450px] gap-4">
+            <img className="w-10" src={assets.icon_user_octagon} alt="" />
+            <div>
+              <p className="text-[#4361ee] mb-2">Budget friendly</p>
+              <p className="text-sm">We prioritize affordability without compromising quality.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative flex gap-4 w-1/2 pt-14">
+        <img
+          className="w-60 h-[432px] rounded-3xl"
+          src={assets.house_1}
+          alt=""
+        />
+        <div className="flex flex-col gap-4">
+          <img className="w-64 h-64 rounded-3xl" src={assets.house_2} alt="" />
+          <img className="w-64 h-40 rounded-3xl" src={assets.house_3} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhoWeAre;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeaderBg from "../components/HeaderBg";
-import Display from "../components/Display";
 import Trustees from "../components/Trustees";
+import WhoWeAre from "../components/WhoWeAre";
 
 const Home = () => {
   return (
@@ -9,6 +9,7 @@ const Home = () => {
       <HeaderBg />
       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
         <Trustees />
+        <WhoWeAre />
       </div>
     </>
   );
