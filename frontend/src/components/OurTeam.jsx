2be17374eb93b33f6deda10cedf160737dea28d0
Joseph Garba <josephgarba97@gmail.com>
2024-11-07 12:23:44 +0100

    Add team members section

--- frontend/src/components/OurTeam.jsx
+++ frontend/src/components/OurTeam.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from "react";
+import Heading from "./Heading";
+import { assets } from "../assets/assets";
 
 const OurTeam = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="my-20">
+      <div className="flex justify-end pr-20">
+        <img className="w-32" src={assets.ellipse_1} alt="" />
+      </div>
+      <div className="text-center mt-5">
+        <Heading text={"INTRODUCE YOURSELF TO"} />
+        <p className="text-4xl font-medium text-[#0b090a] leading-[2.8rem]">
+          Our Team of Experts
+        </p>
+      </div>
 
-export default OurTeam
+      <div className="grid grid-cols-2 gap-4 mt-20 md:flex md:justify-between">
+        <div className="text-center flex flex-col justify-center items-center">
+          <img
+            className="team-img w-[120px] h-[180px] md:w-[200px] md:h-[300px] rounded-full"
+            src={assets.team_4}
+            alt=""
+          />
+          <div>
+            <p className="font-semibold">Brendon M</p>
+            <p className="text-xs text-[#4361ee] mt-2">CEO & Founder</p>
+          </div>
+        </div>
+
+        <div className="text-center flex flex-col justify-center items-center">
+          <img
+            className="team-img w-[120px] h-[180px] md:w-[200px] md:h-[300px] rounded-full"
+            src={assets.team_1}
+            alt=""
+          />
+          <div>
+            <p className="font-semibold">Jodi J. Appleby</p>
+            <p className="text-xs text-[#4361ee] mt-2">Real Estate Developer</p>
+          </div>
+        </div>
+
+        <div className="text-center flex flex-col justify-center items-center">
+          <img
+            className="team-img w-[120px] h-[180px] md:w-[200px] md:h-[300px] rounded-full"
+            src={assets.team_2}
+            alt=""
+          />
+          <div>
+            <p className="font-semibold">Justin S. Meza</p>
+            <p className="text-xs text-[#4361ee] mt-2">Listing Agent</p>
+          </div>
+        </div>
+
+        <div className="text-center flex flex-col justify-center items-center">
+          <img
+            className="team-img w-[120px] h-[180px] md:w-[200px] md:h-[300px] rounded-full"
+            src={assets.team_3}
+            alt=""
+          />
+          <div>
+            <p className="font-semibold">Susan T. Smith</p>
+            <p className="text-xs text-[#4361ee] mt-2">Buyer's Agent</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurTeam;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import WhoWeAre from "../components/WhoWeAre";
 import Latest from "../components/Latest";
 import Services from "../components/Services";
 import PropertiesCount from "../components/PropertiesCount";
+import OurTeam from "../components/OurTeam";
 
 const Home = () => {
   return (
@@ -18,6 +19,7 @@ const Home = () => {
       <Services />
       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
         <PropertiesCount />
+        <OurTeam />
       </div>
     </>
   );
