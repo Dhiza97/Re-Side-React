c1dd607b6961e729be477c2a3002e0f2219d2954
Joseph Garba <josephgarba97@gmail.com>
2024-11-12 12:28:57 +0100

    Update Listings.jsx

--- frontend/src/pages/Listings.jsx
+++ frontend/src/pages/Listings.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import Heading from "../components/Heading";
+import Button from "../components/Button";
 
 const Listings = () => {
   return (
-    <div>
-      
+    <div className='className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] py-10'>
+      <div className="flex justify-between items-center">
+        <p className="text-2xl text-primaryColor space-x-7">OUR PROPERTIES</p>
+        <div className="flex gap-5">
+          <Button text={'All'} />
+          <Button text={'Rent'} />
+          <Button text={'Sale'} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Listings
+export default Listings;
