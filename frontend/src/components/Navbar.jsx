b1af6da9043021c22589b3540cef13caf0f20c7b
Joseph Garba <josephgarba97@gmail.com>
2024-12-16 11:29:46 +0100

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -48,6 +48,8 @@ const Navbar = () => {
             <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 z-10">
               <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-white text-gray-500 rounded">
                 <p className="cursor-pointer hover:text-black">My Profile</p>
+                <p className="cursor-pointer hover:text-black">Tour Bookings</p>
+                <p className="cursor-pointer hover:text-black">Likes</p>
                 <p className="cursor-pointer hover:text-black">Logout</p>
               </div>
             </div>
