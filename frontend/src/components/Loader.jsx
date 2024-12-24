1eb2eede2c150105ea46a50d8f2942cd9e9a00c7
Joseph Garba <josephgarba97@gmail.com>
2024-12-24 02:49:09 +0100

    Update

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Loader = () => {
   return (
     <div className="loader-container">
-      <div className="">
-        <span className="loading loading-ring loading-lg"></span>
+      <div className="loader">
+        <img className="w-20 animate-bounce" src={assets.logo_blue} alt="Loading..." />
       </div>
     </div>
   );
