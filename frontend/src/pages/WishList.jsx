91775c6b39c0ae77a79fa314363107d29a34a2b0
Joseph Garba <josephgarba97@gmail.com>
2025-01-06 16:48:43 +0100

    Update WishList.jsx

--- frontend/src/pages/WishList.jsx
+++ frontend/src/pages/WishList.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { AppContext } from "../context/AppContext";
 import Loader from "../components/Loader";
-import PropertyCard from "../components/PropertyCard"; // Import PropertyCard
+import PropertyCard from "../components/PropertyCard";
 
 const WishList = () => {
   const { properties, fetchLikedProperties, loading, currency } = useContext(AppContext);
@@ -15,8 +15,8 @@ const WishList = () => {
   }
 
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
-      <h1 className="text-2xl font-bold mb-6">My Wishlist</h1>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] min-h-screen">
+      <h1 className="text-2xl text-primaryColor my-10">MY WISHLIST</h1>
       {properties.length > 0 ? (
         <PropertyCard properties={properties} currency={currency} />
       ) : (
