af9d2a722c8fc4146d675a826392a9009d8269c1
Joseph Garba <josephgarba97@gmail.com>
2025-01-08 12:51:45 +0100

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -23,8 +23,6 @@ const AppContextProvider = (props) => {
   // Attach the token to Axios when it is available
   useEffect(() => {
     if (token) {
-      console.log(token);
-      
       try {
         const decoded = jwtDecode(token);
         if (decoded.exp * 1000 < Date.now()) {
