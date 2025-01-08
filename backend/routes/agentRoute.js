b7f69a9c07eb577d22035d705762bef348d45a90
Joseph Garba <josephgarba97@gmail.com>
2025-01-08 13:14:55 +0100

    Update agentRoute.js

--- backend/routes/agentRoute.js
+++ backend/routes/agentRoute.js
@@ -1,12 +1,18 @@
-import express from 'express';
-import { loginAgent, registerAgent } from '../controllers/agentController.js';
+import express from "express";
+import { loginAgent, registerAgent } from "../controllers/agentController.js";
+import { authenticate, authorizeAgent } from "../middleware/auth.js";
 
 const agentRouter = express.Router();
 
 // Register Agent Route
-agentRouter.post('/register', registerAgent)
+agentRouter.post("/register", registerAgent);
 
 // Sign In Agent Route
-agentRouter.post('/login', loginAgent)
+agentRouter.post("/login", loginAgent);
+
+// Get Agent Details Route
+agentRouter.get("/me", authenticate, authorizeAgent, (req, res) => {
+  res.json({ agent: req.user });
+});
 
 export default agentRouter;
