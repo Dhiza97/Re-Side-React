eb64a08643ad1aaaf165e6515139f4bbc3a65737
Joseph Garba <josephgarba97@gmail.com>
2025-01-09 12:50:20 +0100

    Show image

--- frontend/src/pages/MyTourBookings.jsx
+++ frontend/src/pages/MyTourBookings.jsx
@@ -18,11 +18,26 @@ const MyTourBookings = () => {
       <h1 className="text-2xl text-primaryColor my-10">My Tour Bookings</h1>
       {properties.length > 0 ? (
         properties.map((booking) => (
-          <div key={booking._id} className="border p-4 mb-4">
-            <p><strong>Property:</strong> {booking.property.propertyName}</p>
-            <p><strong>Date:</strong> {new Date(booking.date).toLocaleDateString()}</p>
-            <p><strong>Time Slot:</strong> {booking.timeSlot}</p>
-            <p><strong>Status:</strong> {booking.status}</p>
+          <div key={booking._id} className="border p-4 mb-4 flex gap-6">
+            <div>
+              <img className="w-40" src={booking.property.image[0]} alt="" />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <p>
+                <span className="font-medium">Property:</span> {booking.property.propertyName}
+              </p>
+              <p>
+                <span className="font-medium">Date:</span>{" "}
+                {new Date(booking.date).toLocaleDateString()}
+              </p>
+              <p>
+                <span className="font-medium">Time Slot:</span> {booking.timeSlot}
+              </p>
+              <p>
+                <span className="font-medium">Status:</span> {booking.status}
+              </p>
+            </div>
           </div>
         ))
       ) : (
--- frontend/src/pages/agent/TourBookings.jsx
+++ frontend/src/pages/agent/TourBookings.jsx
@@ -33,7 +33,12 @@ const TourBookings = () => {
         properties.map((booking) => (
           <div key={booking._id} className="border p-4 mb-4">
             {booking.property ? (
-              <>
+              <div className="flex gap-6">
+                <div>
+                  <img className="w-40" src={booking.property.image[0]} alt="" />
+                </div>
+
+                <div>
                 <p><strong>Property:</strong> {booking.property.propertyName}</p>
                 <p><strong>Client:</strong> {booking.client.firstName} {booking.client.lastName}</p>
                 <p><strong>Date:</strong> {new Date(booking.date).toLocaleDateString()}</p>
@@ -41,7 +46,8 @@ const TourBookings = () => {
                 <p><strong>Status:</strong> {booking.status}</p>
                 <button onClick={() => updateBookingStatus(booking._id, "accepted")} className="bg-green-500 text-white px-4 py-2 rounded mr-2">Accept</button>
                 <button onClick={() => updateBookingStatus(booking._id, "declined")} className="bg-red-500 text-white px-4 py-2 rounded">Decline</button>
-              </>
+                </div>
+              </div>
             ) : (
               <p className="text-red-500">Property details not available</p>
             )}
