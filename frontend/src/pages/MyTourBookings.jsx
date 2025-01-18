3a4ffa3be6f3b567ff1f20a4dc9ad8572022f220
Joseph Garba <josephgarba97@gmail.com>
2025-01-18 11:13:37 +0100

    Update

--- frontend/src/pages/MyTourBookings.jsx
+++ frontend/src/pages/MyTourBookings.jsx
@@ -18,7 +18,7 @@ const MyTourBookings = () => {
       <h1 className="text-2xl text-primaryColor my-10">MY APPOINTMENTS</h1>
       {properties.length > 0 ? (
         properties.map((booking) => (
-          <div key={booking._id} className="border p-4 mb-4 flex gap-6">
+          <div key={booking._id} className="border p-4 mb-4 flex flex-col sm:flex-row gap-6">
             <img
               className="w-40"
               src={
@@ -29,19 +29,24 @@ const MyTourBookings = () => {
               alt={booking.property?.propertyName || "Property Image"}
             />
 
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-3">
               <p>
                 <span className="font-medium">Property:</span>{" "}
                 {booking.property?.propertyName || "Unknown Property"}
               </p>
-              <p>
-                <span className="font-medium">Date:</span>{" "}
-                {new Date(booking.date).toDateString()}
-              </p>
-              <p>
-                <span className="font-medium">Time Slot:</span>{" "}
-                {booking.timeSlot}
-              </p>
+
+              <div className="font-medium">
+                <p>Date & Time:</p>
+                <div className="flex flex-col sm:flex-row gap-3 text-blue-500 mt-1">
+                  <p className="text-sm font-light flex-shrink-0 px-2 py-2 rounded-full border text-center">
+                    {new Date(booking.date).toDateString()}
+                  </p>
+                  <p className="text-sm font-light flex-shrink-0 px-1 py-2 rounded-full border text-center">
+                    {booking.timeSlot}
+                  </p>
+                </div>
+              </div>
+
               <p>
                 <span className="font-medium">Status:</span>{" "}
                 {booking.status
